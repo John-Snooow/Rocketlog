964103dc3457aa1bba8fd12c98cc820e20b05220
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-06-24 23:32:49 -0300

    Erro
    tive um erro neste projeto e resolvi parar ele por aqui por enquanto, logo eu volto para resolver

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,21 +1,37 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/middlewares/utils/AppError";
 import { hash } from "bcryptjs";
 import { z } from "zod";
 
 class UsersController {
-    async create(request: Request, response: Response) {
-        const bodySchema = z.object({
-            name: z.string().trim().min(2),
-            email: z.string().email(),
-            password: z.string().min(6)
-        })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
 
-        const { name, email, password } = bodySchema.parse(request.body);
+    const { name, email, password } = bodySchema.parse(request.body);
 
-        const hashedPassword = await hash(password, 8)
+    const userWithSameEmail = await prisma.user.findFirst({where: {email}});
 
-        return response.json({ message: "ok", hashedPassword });
+    if (userWithSameEmail) {
+        throw new AppError("User with same email already exists");
     }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return response.json(user);
+  }
 }
 
 export { UsersController };
